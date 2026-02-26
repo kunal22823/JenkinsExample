d184d75f1a9478f3e77534a8dd8329bbcd193e65
Kunal Shinde <113423208+kunal22823@users.noreply.github.com>
2026-02-26 14:32:05 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,6 +1,6 @@
 public class Jenkins {
     public static void main(String[] args) {
-        int rows = 10; 
+        int rows = 7; 
         
         for (int i = 1; i <= rows; i++) {      
             for (int j = 1; j <= i; j++) {
