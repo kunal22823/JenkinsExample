c91533951eab120dfce5ef78117c96ce6dedead3
Kunal Shinde <113423208+kunal22823@users.noreply.github.com>
2026-03-05 14:49:37 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,5 +1,5 @@
 public class Jenkins {
     public static void main(String[] args) {
-                   System.out.println("Atharva Baba ki Jai");           
+                   System.out.println("Kya cheda ");           
     }
 }
